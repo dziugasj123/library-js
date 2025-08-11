0bcf892b19b32dc08bce434d8ab9d9578e68f9e1
Džiugas <jankusdziugas@gmail.com>
2025-08-11 18:07:50 +0300

    change to class

--- script.js
+++ script.js
@@ -1,55 +1,75 @@
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
+class Library{
+  constructor(author, title, pages, read){
     this.id = crypto.randomUUID();
-    this.author = author,
-    this.title = title,
-    this.pages = pages,
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
     this.read = read;
+  }
+
+  set author(newAuthor){
+    this._author = newAuthor
+  };
+  get author(){
+    return this._author
+  };
+  
+  set title(newTitle){
+    this._title = newTitle
+  };
+  get title(){
+    return this._title
+  };
+
+  set pages(newPages){
+    this._pages = newPages
+  };
+  get pages(){
+    return this._pages
+  };
+
+  set read(newRead){
+    this._read = newRead
+  };
+  get read(){
+    return this._read;
+  }
 }
 
-let authorName = '';
-let titleName = '';
-let pagesNumber = 0;
-let readYN = '';
 const table = document.querySelector(".table-class");
 document.querySelector(".book-form").addEventListener("submit", addBookToLibrary);
 
 function addBookToLibrary(event) {
   event.preventDefault();
-  authorName = document.getElementById("author").value;
-  titleName = document.getElementById("title").value;
-  pagesNumber = document.getElementById("pages").value;
-  readYN = document.getElementById("read").value;
+  const newBook = new Library(
+    document.getElementById("author").value,
+    document.getElementById("title").value,
+    document.getElementById("pages").value,
+    document.getElementById("read").value
+  )
   let idValue = '';
 
-  if (!authorName || !titleName || !pagesNumber || !readYN) {
-    alert("Please fill in all fields.");
-    return;
-  }
-
-  myLibrary.push(new Book(authorName, titleName, pagesNumber, readYN));
-  const arrayLength = myLibrary.length;
-  const book = Object.values(myLibrary[arrayLength-1]);
-
+  myLibrary.push(newBook);
+  const bookData = Object.values(newBook);
   const tablerow = document.createElement("tr");
 
-  for (let i = 0; i < book.length+1; i++){
+  for (let i = 0; i < bookData.length+1; i++){
     if (i === 0){
-      idValue = book[0];
+      idValue = bookData[0];
     }
     else {
       const tablehead = document.createElement("th");
-      tablehead.textContent = book[i];
+      tablehead.textContent = bookData[i];
       tablerow.appendChild(tablehead);
-      if (i === book.length){
-        const deleteBookFromArrayButton = document.createElement("button");
-        deleteBookFromArrayButton.className = "delete-book-from-array";
-        deleteBookFromArrayButton.textContent = "X";
-        deleteBookFromArrayButton.addEventListener("click", deleteBookFromLibrary);
-        deleteBookFromArrayButton.dataset.index = idValue;
-        tablehead.appendChild(deleteBookFromArrayButton);
-        tablerow.appendChild(tablehead);
+      if (i === bookData.length){
+        const deleteBook = document.createElement("button");
+        deleteBook.className = "delete-book-from-array";
+        deleteBook.textContent = "X";
+        deleteBook.addEventListener("click", deleteBookFromLibrary);
+        deleteBook.dataset.index = idValue;
+        tablehead.appendChild(deleteBook);
       }
     }
   }
@@ -59,13 +79,10 @@ function addBookToLibrary(event) {
 
 function deleteBookFromLibrary(event){
   const deleteTheBookFromLibraryById = event.target.dataset.index;
-  console.table(myLibrary);
   for (let i = 0; i < myLibrary.length; i++){
     if (myLibrary[i].id === deleteTheBookFromLibraryById){
-      console.log(myLibrary[i].id + " |||||| " + deleteTheBookFromLibraryById);
       myLibrary.splice(i,1);
       table.deleteRow(i+1);
-      console.log(myLibrary.length);
     }
   }
 }
